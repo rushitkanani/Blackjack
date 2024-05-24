2931077be58e026f6636425e715c7fcf11976b6f
RUSHIT PATEL <109105464+rushitkanani@users.noreply.github.com>
2024-05-24 16:19:48 -0400

    user side completed

--- blackjack.py
+++ blackjack.py
@@ -9,16 +9,13 @@ def deal_card():
 
 def calculate_score(cards):
     sum_of_cards = sum(cards)
-    if sum_of_cards == 21:
+    if sum_of_cards == 21 and len(cards)== 2:
         return 0
-    elif sum_of_cards > 21:
-        for i in cards:
-            if i == 11:
-                cards.remove(11)
-                cards.append(1)    
+    if  11 in cards and sum_of_cards > 21:
+        cards.remove(11)
+        cards.append(1)    
         return sum(cards)
-    else:
-        return sum(cards)
+    return sum(cards)
 
 def compare(score_1, score_2):
     if score_1 == score_2:
@@ -35,34 +32,34 @@ def compare(score_1, score_2):
         print("user Wins")
     else:
         print("computer wins")
-a = 0
-while a < 10:
-    user_cards = []
-    computer_cards = []
-    print("--------------NEW GAME--------------------------")
-    for i in range(0,2):
-        user_cards.append(deal_card())
-        computer_cards.append(deal_card())
-    print(f"U:- {user_cards}, C:- {computer_cards}")
 
 
-    if calculate_score(computer_cards)== 21:
-        print("Computer Wins")
-    elif calculate_score(user_cards) == 21:
-        print("User Wins")
+user_cards = []
+computer_cards = []
+is_game_over = False
 
-    ask_user = input("want to draw another card ? ").lower()
-    if ask_user == "yes":
-        user_cards.append(deal_card())
-        
-        calculate_score(user_cards)
-        if calculate_score(computer_cards) < 17:
-            computer_cards.append(deal_card())
-    print(f"user_cards- {user_cards}")
-    print(f"pc_cards- {computer_cards}")
-    print(f"pc_score- {calculate_score(computer_cards)}")
-    print(f"user_score-{calculate_score(user_cards)}")
+for i in range(0,2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
 
-    compare(calculate_score(user_cards),calculate_score(computer_cards))
-    print("--------------GAME  OVER--------------------------")
-    a+=1
+
+
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+
+    print(f"U:- {user_cards} Current user score:- {user_score}, \nC:- {computer_cards[0]}")
+
+    if user_score == 0 or computer_cards == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+        if user_should_deal == "y":
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
+
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
+
